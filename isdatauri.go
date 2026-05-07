d8432d3372040a0894c68d1c40624b27b8ffd03f
bube <attahgbube@gmail.com>
2026-05-07 20:49:05 +0100

    Fix IsDataURI to accept valid data URIs per RFC 2397
    Rewrite implementation to properly parse data URIs: optional MIME type,
    charset parameters, base64 vs URL-encoded data, and + in MIME subtypes
    like image/svg+xml. Fixes 9 previously rejected valid data URIs.

--- isdatauri.go
+++ isdatauri.go
@@ -1,7 +1,14 @@
 package validatorgo
 
 import (
+	"net/url"
 	"regexp"
+	"strings"
+)
+
+var (
+	validAttribute = regexp.MustCompile(`(?i)^[a-z\-]+=[\w\-]+$`)
+	validData      = regexp.MustCompile(`(?i)^[a-z0-9!\$&'()*+,;=\-._~:@/?%\s]*$`)
 )
 
 // A validator that checks if the string is a data uri format.
@@ -11,22 +18,59 @@ import (
 //	ok, _ = validatorgo.IsDataURI("text/plain;base64,SGVsbG8sIFdvcmxkIQ==")
 //	fmt.Println(ok) // false
 func IsDataURI(str string) (bool, error) {
-	re := regexp.MustCompile(`^data:([-\w]+\/[-+\w.]+)?((?:;?[\w]+=[-\w]+)*)(;base64)?,(.*)$`)
+	invalidErr := newValidationError("IsDataURI", ErrInvalidFormat, "invalid datauri")
 
-	capGrp := re.FindStringSubmatch(str)
-
-	if len(capGrp) == 0 {
-		return false, newValidationError("IsDataURI", ErrInvalidFormat, "invalid datauri")
+	commaIdx := strings.Index(str, ",")
+	if commaIdx < 0 {
+		return false, invalidErr
 	}
 
-	mimeTyp := capGrp[1]
-	basePrt := capGrp[4]
+	head := str[:commaIdx]
+	data := str[commaIdx+1:]
 
-	okBase64, _ := IsBase64(basePrt, &IsBase64Opts{UrlSafe: true})
-	okMime, _ := IsMimeType(mimeTyp)
+	attributes := strings.Split(head, ";")
+	schemeAndMediaType := attributes[0]
 
-	if okBase64 && okMime {
-		return true, nil
+	if !strings.HasPrefix(schemeAndMediaType, "data:") {
+		return false, invalidErr
 	}
-	return false, newValidationError("IsDataURI", ErrInvalidFormat, "invalid datauri")
+
+	mediaType := schemeAndMediaType[5:]
+	if mediaType != "" {
+		okMime, _ := IsMimeType(mediaType)
+		if !okMime {
+			return false, invalidErr
+		}
+	}
+
+	isBase64 := false
+	for i := 1; i < len(attributes); i++ {
+		if i == len(attributes)-1 && strings.EqualFold(attributes[i], "base64") {
+			isBase64 = true
+			continue
+		}
+		if !validAttribute.MatchString(attributes[i]) {
+			return false, invalidErr
+		}
+	}
+
+	if isBase64 {
+		decodedData, err := url.PathUnescape(data)
+		if err != nil {
+			return false, invalidErr
+		}
+		okBase64, _ := IsBase64(decodedData, nil)
+		if !okBase64 {
+			return false, invalidErr
+		}
+	} else {
+		if data == "" {
+			return false, invalidErr
+		}
+		if !validData.MatchString(data) {
+			return false, invalidErr
+		}
+	}
+
+	return true, nil
 }
